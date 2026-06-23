493202e6b0c678f2631ded356a2ed1c011e5c669
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-06-23 15:32:34 +0200

    chore: update TnT experiments to use sparse-shot datasets, enable pipeline inference, and add ablation parameters to APS-big config

--- run_experiments_all_tnt.py
+++ run_experiments_all_tnt.py
@@ -11,7 +11,7 @@ SCENES = [
     # TnT Datasets
     {
         "exp_name": "Barn",
-        "data_path": f"{MY_STORAGE}/TNT_GOF/TrainingSet/Barn",
+        "data_path": f"{MY_STORAGE}/TNT_GOF/TrainingSet/Barn_sparseshot",
         "bldg_prompt": "The building",
         "bldg_mask_mode": "biggest",
         "gnd_prompt": ["ground", "grass", "pavement"],
@@ -29,26 +29,18 @@ if __name__ == "__main__":
     token_path = os.path.join(os.path.dirname(__file__), "SAM3", "hf_token.txt")
     set_hf_token_from_txt(token_path)
 
-    # ## SAM3
-    # sam_inference_all_scenes(SCENES)
+    ## SAM3
+    sam_inference_all_scenes(SCENES)
 
-    # ## DA3
-    # da3_inference_all_scenes(SCENES)
+    ## DA3
+    da3_inference_all_scenes(SCENES)
 
     ## PlanarSplatting
     for scene in SCENES:
         run_adaptivePS(
             data_path=scene['data_path'],
             exp_name=scene['exp_name'],
-            out_path="AdaptivePS/TnT",
-            conf_path="configs/APS-Barn.conf",
-            mask="bldg_masks"
-        )
-    for scene in SCENES:
-        run_adaptivePS(
-            data_path=scene['data_path'],
-            exp_name=scene['exp_name'],
-            out_path="AdaptivePS-new/TnT",
-            conf_path="configs/APS-Barn-new.conf",
+            out_path="AdaptivePS-sparse/TnT",
+            conf_path="configs/APS-big.conf",
             mask="bldg_masks"
         )
