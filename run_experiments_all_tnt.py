d52f1148050dc4e0c1b551635cfc895ab1e306f4
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-08-22 13:45:27 +0200

    Run APS with aligned DA3 poses

--- run_experiments_all_tnt.py
+++ run_experiments_all_tnt.py
@@ -2,7 +2,8 @@ import subprocess, sys, os
 # from DA3.inference_colmap import da3_inference_all_scenes, da3_inference_a_scene
 from DA3.inference import da3_inference_all_scenes, da3_inference_a_scene
 from SAM3.inference import set_hf_token_from_txt, sam_inference_all_scenes, sam_inference_a_scene
-from run_APS import run_adaptivePS
+# from run_APS import run_adaptivePS
+from run_APS_sparse import run_adaptivePS
 from run_baseline import run_baseline
 
 
@@ -64,15 +65,15 @@ SCENES = [
         "depth_trunc": 6,
         "max_depth": 200,
     },
-    {
-        "exp_name": "Barn-sparse410",
-        "data_path": f"{MY_STORAGE}/TNT_GOF/TrainingSet/Barn-sparse410",
-        "bldg_prompt": "The building",
-        "bldg_mask_mode": "biggest",
-        "gnd_prompt": ["ground", "grass", "pavement"],
-        "depth_trunc": 6,
-        "max_depth": 200,
-    },
+    # {
+    #     "exp_name": "Barn-sparse410",
+    #     "data_path": f"{MY_STORAGE}/TNT_GOF/TrainingSet/Barn-sparse410",
+    #     "bldg_prompt": "The building",
+    #     "bldg_mask_mode": "biggest",
+    #     "gnd_prompt": ["ground", "grass", "pavement"],
+    #     "depth_trunc": 6,
+    #     "max_depth": 200,
+    # },
 ]
 
 
@@ -88,7 +89,7 @@ if __name__ == "__main__":
     # sam_inference_all_scenes(SCENES)
 
     ## DA3
-    da3_inference_all_scenes(SCENES)
+    # da3_inference_all_scenes(SCENES)
 
     ## PlanarSplatting
     for scene in SCENES:
