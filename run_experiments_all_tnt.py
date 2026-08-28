c518cd77a40129f7865112190fd7e5f2f9f79c2a
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-08-29 00:37:32 +0200

    rerun sparse and regular

--- run_experiments_all_tnt.py
+++ run_experiments_all_tnt.py
@@ -2,8 +2,7 @@ import subprocess, sys, os
 # from DA3.inference_colmap import da3_inference_all_scenes, da3_inference_a_scene
 from DA3.inference import da3_inference_all_scenes, da3_inference_a_scene
 from SAM3.inference import set_hf_token_from_txt, sam_inference_all_scenes, sam_inference_a_scene
-# from run_APS import run_adaptivePS
-from run_APS_sparse import run_adaptivePS
+from run_APS import run_adaptivePS
 from run_baseline import run_baseline
 
 
@@ -91,13 +90,24 @@ if __name__ == "__main__":
     ## DA3
     # da3_inference_all_scenes(SCENES)
 
-    ## PlanarSplatting
+    ## Sparse (with aggressive merger)
     for scene in SCENES:
         run_adaptivePS(
             data_path=scene['data_path'],
             exp_name=scene['exp_name'],
-            out_path="AdaptivePS-sparse-aligned",
-            conf_path="configs/APS-big-sparse.conf",
+            out_path="AdaptivePS-sparse",
+            conf_path="configs/APS-Barn-sparse.conf",
             mask="bldg_masks",
             runtime_log_path="evaluation/runtime_logs_tnt/sparse.json"
+        )
+
+    ## AdaptivePS
+    for scene in SCENES:
+        run_adaptivePS(
+            data_path=scene['data_path'],
+            exp_name=scene['exp_name'],
+            out_path="AdaptivePS",
+            conf_path="configs/APS-Barn.conf",
+            mask="bldg_masks",
+            runtime_log_path="evaluation/runtime_logs_tnt/adaptiveps.json"
         )
